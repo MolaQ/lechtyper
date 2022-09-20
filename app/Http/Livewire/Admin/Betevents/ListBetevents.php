b9b17ac5df7b41d6d2dccadc617ea8f2bfa89886
MolaQ <marcin.molak@gmail.com>
2022-09-20 14:18:20 +0200

    bet events

---
+++ app/Http/Livewire/Admin/Betevents/ListBetevents.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Admin\Betevents;
+
+use Livewire\Component;
+
+class ListBetevents extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.betevents.list-betevents');
+    }
+}
