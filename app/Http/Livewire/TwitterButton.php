8846e26c8b25721dcd888a588dcf9c259d28c66a
MolaQ <marcin.molak@gmail.com>
2022-10-09 10:46:42 +0200

    Twitter Button

---
+++ app/Http/Livewire/TwitterButton.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class TwitterButton extends Component
+{
+    public function render()
+    {
+        return view('livewire.twitter-button');
+    }
+}
