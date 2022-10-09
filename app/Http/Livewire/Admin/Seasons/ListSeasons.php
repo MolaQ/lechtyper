ed7d6b6103a2f9e50c09337b5f790b7db0a5cc5e
MolaQ <marcin.molak@gmail.com>
2022-10-09 10:51:44 +0200

    Season crud

--- app/Http/Livewire/Admin/Seasons/ListSeasons.php
+++ app/Http/Livewire/Admin/Seasons/ListSeasons.php
@@ -3,21 +3,61 @@
 namespace App\Http\Livewire\Admin\Seasons;
 
 use App\Models\Season;
+use Illuminate\Support\Facades\Validator;
 use Livewire\Component;
 
 class ListSeasons extends Component
 {
+    public $state = [];
+    public $showEditModal = false;
+    public $season;
 
     public function addNew()
     {
-        dd("addNew");
+        $this->state = [];
+        $this->showEditModal = false;
+        $this->dispatchBrowserEvent('show-form');
     }
+
+    public function createSeason()
+    {
+        $data = Validator::make($this->state, [
+            'nr' => 'required',
+            'name' => 'required',
+        ])->validate();
+
+        Season::create($data);
+
+        $this->dispatchBrowserEvent('hide-form', ['message' => 'Sezon utworzony!']);
+    }
+
+
 
     public function edit(Season $season)
     {
-        dd($season);
+        $this->showEditModal = true;
+
+        $this->season = $season;
+        $this->state = $season->toArray();
+
+        $this->dispatchBrowserEvent('show-form');
     }
 
+    public function updateSeason()
+    {
+        $data = Validator::make($this->state, [
+            'nr' => 'required',
+            'name' => 'required',
+        ])->validate();
+
+        // if (!empty($data['password'])) {
+        //     $data['password'] = bcrypt($data['password']);
+        // }
+
+        $this->season->update($data);
+
+        $this->dispatchBrowserEvent('hide-form', ['message' => 'Dane sezonu zaktualizowane pomyślnie!']);
+    }
 
     public function render()
     {
