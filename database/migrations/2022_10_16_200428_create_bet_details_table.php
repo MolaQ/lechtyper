2759e255ccb4f28ce087c9490ccd95216e7b6731
MolaQ <marcin.molak@gmail.com>
2022-10-16 22:17:54 +0200

    Bets

---
+++ database/migrations/2022_10_16_200428_create_bet_details_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bet_details', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('footballer_id');
+            $table->integer('time');
+            $table->integer('goals');
+            $table->integer('assists');
+            $table->integer('yellowcards');
+            $table->integer('redcards');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bet_details');
+    }
+};
