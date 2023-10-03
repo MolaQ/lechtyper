91d64782723bcb8d37697fc72a2e17df76b39d46
MolaQ <marcin.molak@gmail.com>
2023-10-03 16:06:04 +0200

    Modal event

--- app/Livewire/Admin/Events/ListEvents.php
+++ app/Livewire/Admin/Events/ListEvents.php
@@ -3,12 +3,15 @@
 namespace App\Livewire\Admin\Events;
 
 use Abraham\TwitterOAuth\TwitterOAuth;
+use DateTime;
 use Livewire\Component;
 
 class ListEvents extends Component
 {
     // public $oauth_token;
     // public $oauth_token_secret;
+
+    public $date;
     public function render()
     {
 
@@ -21,6 +24,8 @@ class ListEvents extends Component
 
         // $status = $connection->post('statuses/update', ['status' => 'To jest mój pierwszy tweet z Laravel!']);
 
+        //$this->date = now()->format("Y-m-d H:i:s");
+ 
         return view('livewire.admin.events.list-events')->layout('components.admin.layouts.app');
     }
 }
